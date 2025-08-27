c595d7abf6169f5ca921d4473d818b0f1c74a102
Bryan Stitt <bryan@stitthappens.com>
2025-08-26 23:22:53 -0700

    save the salt

--- script/salt_finder.sh
+++ script/salt_finder.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -eu -o pipefail
 
-cast create2 \
-    --starts-with "$1" \
-    --init-code-hash "$2" \
-    --no-random | grep Salt | awk '{print $2}' \
-;
+cache_dir=broadcast/salts/
+
+mkdir -p "$cache_dir"
+
+cache_file=$cache_dir/$2-$1
+
+if [ ! -e "$cache_file" ]; then
+    salt=$(cast create2 \
+        --starts-with "$1" \
+        --init-code-hash "$2" \
+        --no-random | grep Salt | awk '{print $2}')
+
+    echo "$salt" > "$cache_file"
+fi
+
+cat "$cache_file"
