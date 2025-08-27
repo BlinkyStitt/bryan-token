55f080e9d66e7c77e851dd7bfa14d60c1ba40ea3
Bryan Stitt <bryan@stitthappens.com>
2025-08-26 23:01:06 -0700

    fix mode

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# originally, i ran `forge script ./script/Bryan.s.sol --account flashprofits --rpc-url https://1rpc.io/base --ffi --broadcast --verify` but that didn't verify the contract.
 set -eux -o pipefail
 
 forge script ./script/Bryan.s.sol \
